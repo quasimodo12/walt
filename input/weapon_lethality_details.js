0c7306e58d5f87bb236178f7e0bf9af4ef3fede8
quasimodo12 <meroryanp@gmail.com>
2025-10-09 06:02:52 -0400

    Update weapon_lethality_details.js

--- input/weapon_lethality_details.js
+++ input/weapon_lethality_details.js
@@ -1,62 +1,62 @@
 var WEAPON_LETHALITY_DATA = [
   {
     "weapon": "BLUE_WEAPON_1",
-    "platformType": "RED_PLATFORM_TYPE_1",
+    "platformType": "RED_DDG_TYPE_1",
     "quantity": 2
   },
   {
     "weapon": "BLUE_WEAPON_1",
-    "platformType": "RED_PLATFORM_TYPE_2",
+    "platformType": "RED_DDG_TYPE_2",
     "quantity": 3
   },
   {
     "weapon": "BLUE_WEAPON_1",
-    "platformType": "RED_PLATFORM_TYPE_3",
+    "platformType": "RED_DDG_TYPE_3",
     "quantity": 4
   },
   {
     "weapon": "BLUE_WEAPON_2",
-    "platformType": "RED_PLATFORM_TYPE_1",
+    "platformType": "RED_DDG_TYPE_1",
     "quantity": 4
   },
   {
     "weapon": "BLUE_WEAPON_2",
-    "platformType": "RED_PLATFORM_TYPE_2",
+    "platformType": "RED_DDG_TYPE_2",
     "quantity": 5
   },
   {
     "weapon": "BLUE_WEAPON_2",
-    "platformType": "RED_PLATFORM_TYPE_3",
+    "platformType": "RED_DDG_TYPE_3",
     "quantity": 6
   },
   {
     "weapon": "BLUE_WEAPON_3",
-    "platformType": "RED_PLATFORM_TYPE_1",
+    "platformType": "RED_DDG_TYPE_1",
     "quantity": 6
   },
   {
     "weapon": "BLUE_WEAPON_3",
-    "platformType": "RED_PLATFORM_TYPE_2",
+    "platformType": "RED_DDG_TYPE_2",
     "quantity": 7
   },
   {
     "weapon": "BLUE_WEAPON_3",
-    "platformType": "RED_PLATFORM_TYPE_3",
+    "platformType": "RED_DDG_TYPE_3",
     "quantity": 8
   },
   {
     "weapon": "BLUE_WEAPON_4",
-    "platformType": "RED_PLATFORM_TYPE_1",
+    "platformType": "RED_DDG_TYPE_1",
     "quantity": 3
   },
   {
     "weapon": "BLUE_WEAPON_4",
-    "platformType": "RED_PLATFORM_TYPE_2",
+    "platformType": "RED_DDG_TYPE_2",
     "quantity": 4
   },
   {
     "weapon": "BLUE_WEAPON_4",
-    "platformType": "RED_PLATFORM_TYPE_3",
+    "platformType": "RED_DDG_TYPE_3",
     "quantity": 5
   }
 ];
