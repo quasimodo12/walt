a912badf87daae6f35e4c5b122720210e3a864ed
quasimodo12 <meroryanp@gmail.com>
2026-09-10 12:02:34 +0000

    Changed default range ring opacity

--- input/range_ring_style_templates.js
+++ input/range_ring_style_templates.js
@@ -3,12 +3,12 @@ var range_ring_style_templates = [
     name: 'Blue Default',
     color: '#2f80ed',
     lineWidth: 2,
-    opacity: 0.35
+    opacity: 1.0
   },
   {
     name: 'Red Default',
     color: '#eb5757',
     lineWidth: 2,
-    opacity: 0.35
+    opacity: 1.0
   }
 ];
